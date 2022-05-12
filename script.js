ea182eae5eaa865ab0b6223909f56e699953d30e
ParseThisCode <kamilochman79@gmail.com>
2022-05-12 13:00:59 +0200

    1 file modified, 2 added

---
+++ script.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        dark: '#212121',
+        fb: '#369',
+      },
+    },
+  },
+};
+document.addEventListener('contextmenu', (event) => event.preventDefault());
